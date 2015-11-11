764cd92730df898b1d7b2b148f94da64224339a2
Daniel Scherzer <scherzer@hs-weingarten.de>
2015-11-11 18:46:34 +0100

    initial copy from cg/Framework

---
+++ Texture.cs
@@ -0,0 +1,87 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+
+namespace Framework
+{
+	/// <summary>
+	/// Gl Texture class that allows loading from a file.
+	/// </summary>
+	public class Texture : IDisposable
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Texture"/> class.
+		/// </summary>
+		public Texture()
+		{
+			//generate one texture and put its ID number into the "m_uTextureID" variable
+			GL.GenTextures(1, out m_uTextureID);
+			Width = 0;
+			Height = 0;
+		}
+
+		public void Dispose()
+		{
+			GL.DeleteTexture(m_uTextureID);
+		}
+
+		/// <summary>
+		/// Filters the texture with a point filter.
+		/// </summary>
+		public void FilterBilinear()
+		{
+			BeginUse();
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (float)TextureMagFilter.Linear);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (float)TextureMinFilter.Linear);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.GenerateMipmap, 0);
+			EndUse();
+		}
+
+		public void FilterNearest()
+		{
+			BeginUse();
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (float)TextureMagFilter.Nearest);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (float)TextureMinFilter.Nearest);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.GenerateMipmap, 0);
+			EndUse();
+		}
+
+		/// <summary>
+		/// Filters the texture with a tent filter.
+		/// </summary>
+		public void FilterTrilinear()
+		{
+			BeginUse();
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (float)TextureMagFilter.Linear);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (float)TextureMinFilter.LinearMipmapLinear);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.GenerateMipmap, 1);
+			EndUse();
+		}
+
+		public void BeginUse()
+		{
+			GL.Enable(EnableCap.Texture2D);
+			GL.BindTexture(TextureTarget.Texture2D, m_uTextureID);
+		}
+
+		public void EndUse()
+		{ 
+			GL.Disable(EnableCap.Texture2D);
+		}
+
+		public void LoadPixels(IntPtr pixels, int width, int height, PixelInternalFormat internalFormat, OpenTK.Graphics.OpenGL.PixelFormat inputPixelFormat)
+		{
+			GL.TexImage2D(TextureTarget.Texture2D, 0, internalFormat, width, height, 0,
+				inputPixelFormat, PixelType.UnsignedByte, pixels);
+			this.Width = width;
+			this.Height = height;
+		}
+
+		public int Width { get; private set; }
+
+		public int Height { get; private set; }
+
+		public uint ID { get { return this.m_uTextureID; } }
+	
+		private readonly uint m_uTextureID = 0;
+	}
+}
---
+++ TextureLoader.cs
@@ -0,0 +1,56 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Framework
+{
+	public class TextureLoader
+	{
+		public static Texture FromFile(string fileName)
+		{
+			if (String.IsNullOrEmpty(fileName))
+			{
+				throw new ArgumentException(fileName);
+			}
+			if (!File.Exists(fileName))
+			{
+				throw new FileLoadException(fileName);
+			}
+
+			Texture texture = new Texture();
+			texture.BeginUse();
+			texture.FilterTrilinear();
+			Bitmap bmp = new Bitmap(fileName);
+			bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+			BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
+			PixelInternalFormat internalFormat = selectInternalPixelFormat(bmp.PixelFormat);
+			OpenTK.Graphics.OpenGL.PixelFormat inputPixelFormat = selectInputPixelFormat(bmp.PixelFormat);
+			texture.LoadPixels(bmpData.Scan0, bmpData.Width, bmpData.Height, internalFormat, inputPixelFormat);
+			bmp.UnlockBits(bmpData);
+			texture.EndUse();
+			return texture;
+		}
+
+		private static OpenTK.Graphics.OpenGL.PixelFormat selectInputPixelFormat(System.Drawing.Imaging.PixelFormat pixelFormat)
+		{
+			switch (pixelFormat)
+			{
+				case System.Drawing.Imaging.PixelFormat.Format24bppRgb: return OpenTK.Graphics.OpenGL.PixelFormat.Bgr;
+				case System.Drawing.Imaging.PixelFormat.Format32bppArgb: return OpenTK.Graphics.OpenGL.PixelFormat.Bgra;
+				default: throw new FileLoadException("Wrong pixel format " + pixelFormat.ToString());
+			}
+		}
+
+		private static PixelInternalFormat selectInternalPixelFormat(System.Drawing.Imaging.PixelFormat pixelFormat)
+		{
+			switch (pixelFormat)
+			{
+				case System.Drawing.Imaging.PixelFormat.Format24bppRgb: return PixelInternalFormat.Rgb;
+				case System.Drawing.Imaging.PixelFormat.Format32bppArgb: return PixelInternalFormat.Rgba;
+				default: throw new FileLoadException("Wrong pixel format " + pixelFormat.ToString());
+			}
+		}
+	}
+}
