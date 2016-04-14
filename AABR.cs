d045596bf2c43f1acb6f3e9c5d77ec16d88b3f14
Matthias Mettenleiter <MettenleiterMatthias@web.de>
2016-04-14 14:01:36 +0200

    Changed indent

--- AABR.cs
+++ AABR.cs
@@ -38,31 +38,31 @@
 			return !(noXintersect || noYintersect);
 		}
 
-        /// <summary>
-        /// Calculates the AABR in the overlap
-        /// Returns null if no intersection
-        /// </summary>
-        /// <param name="frame"></param>
-        /// <returns>AABR in the overlap</returns>
-        public AABR Overlap(AABR frame)
-        {
-            AABR overlap = null;
+        	/// <summary>
+        	/// Calculates the AABR in the overlap
+        	/// Returns null if no intersection
+        	/// </summary>
+        	/// <param name="frame"></param>
+        	/// <returns>AABR in the overlap</returns>
+        	public AABR Overlap(AABR frame)
+        	{
+            	AABR overlap = null;
 
-            if (Intersects(frame))
-            {
-                overlap = new AABR(0.0f, 0.0f, 0.0f, 0.0f);
+            	if (Intersects(frame))
+            	{
+                	overlap = new AABR(0.0f, 0.0f, 0.0f, 0.0f);
 
-                overlap.X = (X < frame.X) ? frame.X : X;
-                overlap.Y = (Y < frame.Y) ? frame.Y : Y;
+                	overlap.X = (X < frame.X) ? frame.X : X;
+                	overlap.Y = (Y < frame.Y) ? frame.Y : Y;
 
-                overlap.SizeX = (MaxX < frame.MaxX) ? MaxX - overlap.X : frame.MaxX - overlap.X;
-                overlap.SizeY = (MaxY < frame.MaxY) ? MaxY - overlap.Y : frame.MaxY - overlap.Y;
-            }
+                	overlap.SizeX = (MaxX < frame.MaxX) ? MaxX - overlap.X : frame.MaxX - overlap.X;
+                	overlap.SizeY = (MaxY < frame.MaxY) ? MaxY - overlap.Y : frame.MaxY - overlap.Y;
+            	}
 
-            return overlap;
-        }
+            	return overlap;
+        	}
 
-        public float MaxX { get { return X + SizeX; } }
+        	public float MaxX { get { return X + SizeX; } }
 
 		public float MaxY { get { return Y + SizeY; } }
 
