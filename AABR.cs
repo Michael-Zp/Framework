dea146cd975c7110a2383de2c26d3f98a38b236d
MMDaemon <mettenleitermatthias@web.de>
2016-04-14 14:14:29 +0200

    Changed indent

--- AABR.cs
+++ AABR.cs
@@ -1,75 +1,75 @@
 ﻿namespace Framework
 {
-	public class AABR
-	{
-		public AABR(float x, float y, float sizeX, float sizeY)
-		{
-			this.X = x;
-			this.Y = y;
-			this.SizeX = sizeX;
-			this.SizeY = sizeY;
-		}
+    public class AABR
+    {
+        public AABR(float x, float y, float sizeX, float sizeY)
+        {
+            this.X = x;
+            this.Y = y;
+            this.SizeX = sizeX;
+            this.SizeY = sizeY;
+        }
 
-		public AABR(AABR aabr)
-		{
-			this.X = aabr.X;
-			this.Y = aabr.Y;
-			this.SizeX = aabr.SizeX;
-			this.SizeY = aabr.SizeY;
-		}
+        public AABR(AABR aabr)
+        {
+            this.X = aabr.X;
+            this.Y = aabr.Y;
+            this.SizeX = aabr.SizeX;
+            this.SizeY = aabr.SizeY;
+        }
 
-		public float SizeX { get; set; }
-	
-		public float SizeY { get; set; }
-		
-		public float X { get; set; }
-		
-		public float Y { get; set; }
+        public float SizeX { get; set; }
 
-		public float CenterX { get { return X + 0.5f * SizeX; } }
+        public float SizeY { get; set; }
 
-		public float CenterY { get { return Y + 0.5f * SizeY; } }
+        public float X { get; set; }
 
-		public bool Intersects(AABR frame)
-		{
-			if (null == frame) return false;
-			bool noXintersect = (MaxX < frame.X) || (X > frame.MaxX);
-			bool noYintersect = (MaxY < frame.Y) || (Y > frame.MaxY);
-			return !(noXintersect || noYintersect);
-		}
+        public float Y { get; set; }
 
-        	/// <summary>
-        	/// Calculates the AABR in the overlap
-        	/// Returns null if no intersection
-        	/// </summary>
-        	/// <param name="frame"></param>
-        	/// <returns>AABR in the overlap</returns>
-        	public AABR Overlap(AABR frame)
-        	{
-            		AABR overlap = null;
+        public float CenterX { get { return X + 0.5f * SizeX; } }
 
-            		if (Intersects(frame))
-            		{
-                		overlap = new AABR(0.0f, 0.0f, 0.0f, 0.0f);
+        public float CenterY { get { return Y + 0.5f * SizeY; } }
 
-                		overlap.X = (X < frame.X) ? frame.X : X;
-                		overlap.Y = (Y < frame.Y) ? frame.Y : Y;
+        public bool Intersects(AABR frame)
+        {
+            if (null == frame) return false;
+            bool noXintersect = (MaxX < frame.X) || (X > frame.MaxX);
+            bool noYintersect = (MaxY < frame.Y) || (Y > frame.MaxY);
+            return !(noXintersect || noYintersect);
+        }
 
-                		overlap.SizeX = (MaxX < frame.MaxX) ? MaxX - overlap.X : frame.MaxX - overlap.X;
-                		overlap.SizeY = (MaxY < frame.MaxY) ? MaxY - overlap.Y : frame.MaxY - overlap.Y;
-            		}
+        /// <summary>
+        /// Calculates the AABR in the overlap
+        /// Returns null if no intersection
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <returns>AABR in the overlap</returns>
+        public AABR Overlap(AABR frame)
+        {
+            AABR overlap = null;
 
-            		return overlap;
-        	}
+            if (Intersects(frame))
+            {
+                overlap = new AABR(0.0f, 0.0f, 0.0f, 0.0f);
 
-        	public float MaxX { get { return X + SizeX; } }
+                overlap.X = (X < frame.X) ? frame.X : X;
+                overlap.Y = (Y < frame.Y) ? frame.Y : Y;
 
-		public float MaxY { get { return Y + SizeY; } }
+                overlap.SizeX = (MaxX < frame.MaxX) ? MaxX - overlap.X : frame.MaxX - overlap.X;
+                overlap.SizeY = (MaxY < frame.MaxY) ? MaxY - overlap.Y : frame.MaxY - overlap.Y;
+            }
 
-		public override string ToString()
-		{
-			return '(' + X.ToString() + ';' + Y.ToString() + ';' + SizeX.ToString() + ';' + SizeY .ToString() + ')';
-		}
+            return overlap;
+        }
 
-	}
+        public float MaxX { get { return X + SizeX; } }
+
+        public float MaxY { get { return Y + SizeY; } }
+
+        public override string ToString()
+        {
+            return '(' + X.ToString() + ';' + Y.ToString() + ';' + SizeX.ToString() + ';' + SizeY.ToString() + ')';
+        }
+
+    }
 }
