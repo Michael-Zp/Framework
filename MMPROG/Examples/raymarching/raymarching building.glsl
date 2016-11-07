bbe446de470494949781cf7d6e966a2a028ab7ac
Daniel Scherzer <scherzer@hs-weingarten.de>
2016-11-07 10:43:21 +0100

    mmprog ambient occlusion

--- MMPROG/Examples/raymarching/raymarching building.glsl
+++ MMPROG/Examples/raymarching/raymarching building.glsl
@@ -1,86 +1,40 @@
 #version 330
 
 #include "libs/camera.glsl"
+#include "libs/operators.glsl"
 
 uniform vec2 iResolution;
 
 const float epsilon = 0.0001;
-const int maxSteps = 128;
+const int maxSteps = 512;
 
-float vmax(vec3 v) {
-	return max(max(v.x, v.y), v.z);
-}
-
-float uBox(vec3 point, vec3 center, vec3 b )
-{
-  return length(max(abs(point - center) - b, vec3(0.0)));
-}
-
-float sBox(vec3 point, vec3 center, vec3 b) {
-	vec3 d = abs(point - center) - b;
-	return length(max(d, vec3(0))) + vmax(min(d, vec3(0)));
-}
-
-float sSphere(vec3 point, vec3 center, float radius) {
-    return length(point - center) - radius;
-}
-
-float uSphere(vec3 point, vec3 center, float radius) {
-    return max(0.0, sSphere(point, center, radius));
-}
-
-float opUnion(float dist1, float dist2)
-{
-	return min(dist1, dist2);
-}
-
-float opIntersection(float dist1, float dist2)
-{
-	return max(dist1, dist2);
-}
-
-float opDifference(float dist1, float dist2)
-{
-	return max(dist1, -dist2);
-}
-
-vec3 opRepeat(vec3 point, vec3 interval) {
-	vec3 c = floor((point + interval*0.5)/interval);
-	return mod(point + interval*0.5, interval) - interval*0.5;
-}
-
-float distFunc(vec3 point)
+float distField(vec3 point)
 {
 	float dist1 = sBox(point, vec3(0, 0, 0), vec3(10, 10, 10));
-	vec3 repXY = opRepeat(point, vec3(.5, .5, 1));
+	vec3 repXY = opRepeatCentered(point, vec3(.5, .5, 1));
 	float dist2 = sBox(repXY, vec3(0, 0, 0), vec3(0.1, 0.1, 10));
-	vec3 repYZ = opRepeat(point, vec3(1, .5, .5));
+	vec3 repYZ = opRepeatCentered(point, vec3(1, .5, .5));
 	float dist3 = sBox(repYZ, vec3(0, 0, 0), vec3(10, 0.1, 0.1));
 	return opDifference(dist1, opUnion(dist2, dist3));
 }
 
-//by numerical gradient
-vec3 getNormal(vec3 point)
+float ambientOcclusion(vec3 point, float delta, int samples)
 {
-	float d = epsilon;
-	//get points a little bit to each side of the point
-	vec3 right = point + vec3(d, 0.0, 0.0);
-	vec3 left = point + vec3(-d, 0.0, 0.0);
-	vec3 up = point + vec3(0.0, d, 0.0);
-	vec3 down = point + vec3(0.0, -d, 0.0);
-	vec3 behind = point + vec3(0.0, 0.0, d);
-	vec3 before = point + vec3(0.0, 0.0, -d);
-	//calc difference of distance function values == numerical gradient
-	vec3 gradient = vec3(distFunc(right) - distFunc(left),
-		distFunc(up) - distFunc(down),
-		distFunc(behind) - distFunc(before));
-	return normalize(gradient);
+	vec3 normal = getNormal(point, 0.0001);
+	float occ = 0;
+	for(int i = 1; i < samples; ++i)
+	{
+		occ += (i * delta - distField(point + i * delta * normal));
+	}
+	// occ /= samples;
+	return 1 - occ;
 }
 
 void main()
 {
 	vec3 camP = calcCameraPos();
-	camP.z += -1.0;
+	camP.x += 11.0;
+	camP.z += -12.0;
 	vec3 camDir = calcCameraRayDir(80.0, gl_FragCoord.xy, iResolution);
 
 	//start point is the camera position
@@ -88,34 +42,46 @@ void main()
 	bool objectHit = false;
 	float t = 0.0;
 	//step along the ray 
-    for(int steps = 0; steps < maxSteps; ++steps)
+	int steps = 0;
+    for(; (steps < maxSteps)&& (t < 10); ++steps)
     {
 		//check how far the point is from the nearest surface
-        float dist = distFunc(point);
+        float dist = distField(point);
 		//if we are very close
         if(epsilon > dist)
         {
 			objectHit = true;
             break;
         }
+		//screen error decreases with distance
+		// dist = max(dist, t * 0.001);
 		//not so close -> we can step at least dist without hitting anything
-        t += dist;
+		t += dist;
+
 		//calculate new point
         point = camP + t * camDir;
     }
 
+	float effort = steps;
+	effort /= maxSteps;
+	vec3 red = vec3(1, 0, 0);
+	vec3 green = vec3(0, 1, 0);
+	vec3 color = mix(green, red, effort);
 	if(objectHit)
 	{
-		vec3 normal = getNormal(point);
-		vec3 lightDir = normalize(vec3(.5,-1,.5));
-		float diffuse = max(0, dot(-lightDir, normal));
-		vec3 color = vec3(1); //white
+		vec3 normal = getNormal(point, 0.01);
+		vec3 lightDir = normalize(vec3(1, -1.0, 1));
+		vec3 toLight = -lightDir;
+		float diffuse = max(0, dot(toLight, normal));
+		vec3 material = vec3(1); //white
 		vec3 ambient = vec3(0.2);
 
-		gl_FragColor = vec4(ambient + diffuse * color, 1);
+		color = ambient + diffuse * material;	
+		color = ambientOcclusion(point, 0.02, 5) * color;
 	}
 	else
 	{
-		gl_FragColor = vec4(0, 0, 1, 1);
+		color = vec3(0);
 	}
+	gl_FragColor = vec4(color, 1);
 }
