46ae43f8fb9d976653a02c6d5cb48bf8240fbd85
Daniel Scherzer <scherzer@hs-weingarten.de>
2017-02-09 13:39:23 +0100

    merged solutions into one master solution for all lectures

---
+++ CG/Examples/RasterizationExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Drawing;
+
+namespace Example
+{
+	class MyApplication
+	{
+		private Rasterizer rasterizer;
+
+		[STAThread]
+		public static void Main()
+		{
+			var app = new MyApplication();
+			//run the update loop, which calls our draw callback
+			app.rasterizer.Run();
+		}
+
+		private MyApplication()
+		{
+			rasterizer = new Rasterizer(20, 20, Draw);
+		}
+
+		private void Draw()
+		{
+			GL.Color3(0.2, 0.4, 1);
+			GL.Begin(PrimitiveType.Lines);
+				GL.Vertex2(-1, -1);
+				GL.Vertex2(1, 1);
+			GL.End();
+		}
+	}
+}
