a8aea40281425bff982714a4b143b43b50431bf9
Daniel Scherzer <scherzer@hs-weingarten.de>
2017-03-17 14:56:23 +0100

    quick-commit

--- CG/Examples/MinimalExample/MyWindow.cs
+++ CG/Examples/MinimalExample/MyWindow.cs
@@ -15,14 +15,13 @@ namespace Example
 			//clear screen - what happens without?
 			GL.Clear(ClearBufferMask.ColorBufferBit);
 			//draw a primitive
-			GL.Begin(PrimitiveType.Polygon);
+			GL.Begin(PrimitiveType.Quads);
 			//color is active as long as no new color is set
 			GL.Color3(Color.Cyan);
 			GL.Vertex2(0.0f, 0.0f);
 			GL.Vertex2(0.5f, 0.0f);
 			GL.Color3(Color.White);
 			GL.Vertex2(0.5f, 0.5f);
-			GL.Vertex2(0.3f, 0.7f);
 			GL.Vertex2(0.0f, 0.5f);
 			GL.End();
 		}
--- CG/Examples/MinimalExample/readme.md
+++ CG/Examples/MinimalExample/readme.md
@@ -3,7 +3,8 @@
 1. Give each vertex a different color
 1. Execute and compare
 	+ Note that OpenGL is a state machine. Color stays until other color is selected.
-1. Make one corner of the polygon move to the right over time
-1. Make the whole polygon move to the right over time
+1. Make the whole quad move to the right over time
+1. Put the draw code for the quad into a function
+	1. Use Box2D as function parameter
 1. many enemies
 1. keyboard player movement
