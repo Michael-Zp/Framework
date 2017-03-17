147feecfb9b14769861c2105cdd8efce89c31d8f
Daniel Scherzer <scherzer@hs-weingarten.de>
2017-03-17 11:37:25 +0100

    quick-commit

---
+++ CG/Examples/MinimalExample/MyWindow.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace Example
+{
+	public class MyWindow
+	{
+		public void Update(float updatePeriod)
+		{
+			//todo: ask for key with Keyboard.GetState()[Key.Left]
+		}
+
+		public void Render()
+		{
+			//clear screen - what happens without?
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+			//draw a primitive
+			GL.Begin(PrimitiveType.Quads);
+			//set color color is active as long as no other color is set
+			GL.Color3(Color.White);
+			GL.Vertex2(0.0f, 0.0f);
+			GL.Vertex2(0.5f, 0.0f);
+			GL.Vertex2(0.5f, 0.5f);
+			GL.Vertex2(0.0f, 0.5f);
+			GL.End();
+		}
+	}
+}
