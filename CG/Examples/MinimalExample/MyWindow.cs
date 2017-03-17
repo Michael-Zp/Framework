ec444f1b96ea221d1382ec66812049b78526ad0c
Daniel Scherzer <scherzer@hs-weingarten.de>
2017-03-17 14:42:40 +0100

    quick-commit

--- CG/Examples/MinimalExample/MyWindow.cs
+++ CG/Examples/MinimalExample/MyWindow.cs
@@ -6,14 +6,8 @@ namespace Example
 {
 	public class MyWindow
 	{
-		float x = -1;
-		float delta = 0.01f;
-
 		public void Update(float updatePeriod)
 		{
-			//todo: ask for key with Keyboard.GetState()[Key.Left]
-			if (x > 0.5f || x < -1) delta = -delta;
-			x += delta;
 		}
 
 		public void Render()
@@ -24,13 +18,10 @@ namespace Example
 			GL.Begin(PrimitiveType.Quads);
 			//set color color is active as long as no other color is set
 			GL.Color3(Color.Cyan);
-			GL.Vertex2(x, 0.0f);
-			GL.Color3(Color.Chartreuse);
-			GL.Vertex2(x + 0.5f, 0.0f);
-			GL.Color3(Color.Coral);
-			GL.Vertex2(x + 0.1f, 0.7f);
-			GL.Color3(Color.Red);
-			GL.Vertex2(x, 0.5f);
+			GL.Vertex2(0.0f, 0.0f);
+			GL.Vertex2(0.5f, 0.0f);
+			GL.Vertex2(0.1f, 0.7f);
+			GL.Vertex2(0.0f, 0.5f);
 			GL.End();
 		}
 	}
