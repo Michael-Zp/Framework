418ac62f1b1c8af9e8bc2eae3fa580d69e9fdb21
Daniel Scherzer <scherzer@hs-weingarten.de>
2017-03-17 14:13:22 +0100

    quick-commit

--- CG/Examples/MinimalExample/MyWindow.cs
+++ CG/Examples/MinimalExample/MyWindow.cs
@@ -1,13 +1,19 @@
-﻿using OpenTK.Graphics.OpenGL;
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
 using System.Drawing;
 
 namespace Example
 {
 	public class MyWindow
 	{
+		float x = -1;
+		float delta = 0.01f;
+
 		public void Update(float updatePeriod)
 		{
 			//todo: ask for key with Keyboard.GetState()[Key.Left]
+			if (x > 0.5f || x < -1) delta = -delta;
+			x += delta;
 		}
 
 		public void Render()
@@ -17,11 +23,14 @@ namespace Example
 			//draw a primitive
 			GL.Begin(PrimitiveType.Quads);
 			//set color color is active as long as no other color is set
-			GL.Color3(Color.White);
-			GL.Vertex2(0.0f, 0.0f);
-			GL.Vertex2(0.5f, 0.0f);
-			GL.Vertex2(0.5f, 0.5f);
-			GL.Vertex2(0.0f, 0.5f);
+			GL.Color3(Color.Cyan);
+			GL.Vertex2(x, 0.0f);
+			GL.Color3(Color.Chartreuse);
+			GL.Vertex2(x + 0.5f, 0.0f);
+			GL.Color3(Color.Coral);
+			GL.Vertex2(x + 0.1f, 0.7f);
+			GL.Color3(Color.Red);
+			GL.Vertex2(x, 0.5f);
 			GL.End();
 		}
 	}
--- readme.md
+++ readme.md
@@ -1,15 +1,16 @@
 ### [Game events calendar](https://goo.gl/SySLwF)
 
 ### Setup of framework
-+ repository clone [TortoiseGit](https://tortoisegit.org/)
-	1. create empty dir
-	1. right click <git clone...>
-	1. URL: https://github.com/danielscherzer/Framework
-+ repository clone (shell)
-    1. open git cmd / git bash
-    1. change to empty directory
-    1. git clone https://github.com/danielscherzer/Framework
-    1. compile and run
+1. Download framework
+	+ repository clone [TortoiseGit](https://tortoisegit.org/)
+		1. create empty dir
+		1. right click <git clone...>
+		1. URL: https://github.com/danielscherzer/Framework
+	+ repository clone (shell)
+		1. open git cmd / git bash
+		1. change to empty directory
+		1. git clone https://github.com/danielscherzer/Framework
+1. compile and run
 	
 ### Understanding versioning with git
 + [What is version control?](https://de.atlassian.com/git/tutorials/what-is-version-control)
