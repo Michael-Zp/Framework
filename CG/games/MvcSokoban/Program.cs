c71b9fdd7b50778f785448fce019ba85de0fdcd2
scherzer <scherzer@hs-weingarten.de>
2017-07-31 13:06:44 +0200

    quick-commit

--- CG/games/MvcSokoban/Program.cs
+++ CG/games/MvcSokoban/Program.cs
@@ -1,83 +1,51 @@
-﻿using OpenTK;
+﻿using DMS.Application;
 using OpenTK.Input;
 using System;
 
 namespace MvcSokoban
 {
-	class MyApplication
+	class Controller
 	{
 		[STAThread]
 		private static void Main()
 		{
-			MyApplication app = new MyApplication();
+			var app = new ExampleApplication();
+			uint levelNr = 1;
+			var logic = LoadLevel(levelNr);
+			var visual = new Visual();
+			app.Update += (t) =>
+			{
+				if (logic.GetLevel().IsWon())
+				{
+					++levelNr;
+					logic = LoadLevel(levelNr);
+				}
+			};
+			app.GameWindow.KeyDown += (s, e) =>
+			{
+				switch (e.Key)
+				{
+					case Key.Escape: app.GameWindow.Close(); break;
+					case Key.R: logic = LoadLevel(levelNr); break;
+					case Key.N: ++levelNr; logic = LoadLevel(levelNr); break;
+					case Key.Left: logic.Update(GameLogic.Movement.LEFT); break;
+					case Key.Right: logic.Update(GameLogic.Movement.RIGHT); break;
+					case Key.Up: logic.Update(GameLogic.Movement.UP); break;
+					case Key.Down: logic.Update(GameLogic.Movement.DOWN); break;
+				};
+			};
+			app.Resize += (w, h) => visual.ResizeWindow(w, h);
+			app.Render += () => visual.DrawScreen(logic.GetLevel());
+			Controller controller = new Controller();
+			app.Run();
 		}
 
-		public MyApplication()
-		{
-			gameWindow = new GameWindow(800, 800);
-			visual = new Visual();
-			gameWindow.Load += GameWindow_Load;
-			gameWindow.Resize += GameWindow_Resize;
-			gameWindow.KeyDown += GameWindow_KeyDown;
-			gameWindow.UpdateFrame += GameWindow_UpdateFrame;
-			gameWindow.RenderFrame += GameWindow_RenderFrame;
-			gameWindow.RenderFrame += (sender, e) => { gameWindow.SwapBuffers(); };
-			LoadLevel();
-			gameWindow.Run(60.0);
-		}
-
-		private uint levelNr = 1;
-
-		private void LoadLevel()
+		private static GameLogic LoadLevel(uint levelNr)
 		{
 			var levelString = Resourcen.ResourceManager.GetString("level" + levelNr.ToString());
 			var level = LevelLoader.FromString(levelString);
-			if (ReferenceEquals(null, level)) return;
-			logic = new GameLogic(level);
+			if (ReferenceEquals(null, level)) return null;
+			return new GameLogic(level);
 		}
-
-		private void GameWindow_KeyDown(object sender, KeyboardKeyEventArgs e)
-		{
-			switch (e.Key)
-			{
-				case Key.Escape: gameWindow.Exit(); break;
-				case Key.R: LoadLevel(); break;
-				case Key.N: ++levelNr; LoadLevel(); break;
-				case Key.Left: logic.Update(GameLogic.Movement.LEFT); break;
-				case Key.Right: logic.Update(GameLogic.Movement.RIGHT); break;
-				case Key.Up: logic.Update(GameLogic.Movement.UP); break;
-				case Key.Down: logic.Update(GameLogic.Movement.DOWN); break;
-			};
-		}
-
-		private void GameWindow_Load(object sender, EventArgs e)
-		{
-			//gameWindow.WindowState = WindowState.Fullscreen;
-			gameWindow.VSync = VSyncMode.On;
-		}
-
-		private void GameWindow_Resize(object sender, EventArgs e)
-		{
-			visual.ResizeWindow(gameWindow.Width, gameWindow.Height);
-		}
-
-		private void GameWindow_UpdateFrame(object sender, FrameEventArgs e)
-		{
-			float updatePeriod = (float)gameWindow.UpdatePeriod;
-			if (logic.GetLevel().IsWon())
-			{
-				++levelNr;
-				LoadLevel();
-			}
-		}
-
-		private void GameWindow_RenderFrame(object sender, FrameEventArgs e)
-		{
-			visual.DrawScreen(logic.GetLevel());
-		}
-
-		private GameWindow gameWindow;
-		private GameLogic logic;
-		private Visual visual;
 	}
 }
--- CG/games/MvcSokoban/Visual.cs
+++ CG/games/MvcSokoban/Visual.cs
@@ -9,7 +9,20 @@ namespace MvcSokoban
 	{
 		public Visual()
 		{
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.GroundGravel_Grass));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.Character4));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.Crate_Brown));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.Crate_Brown));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.EndPoint_Red));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.EndPointCharacter));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.Crate_Brown));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.EndPointCrate_Brown));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.Wall_Beige));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.Wall_Beige));
+			tileSet.AddFrame(TextureLoader.FromBitmap(Resourcen.Wall_Beige));
+
 			//this.spriteSheet = new SpriteSheet(TextureLoader.FromBitmap(Resourcen.skin36), 4, 0.98f, 0.97f);
+
 			this.spriteSheet = new SpriteSheet(TextureLoader.FromBitmap(Resourcen.borgar), 4, 0.95f, 0.95f);
 		}
 
@@ -17,32 +30,54 @@ namespace MvcSokoban
 		{
 			GL.MatrixMode(MatrixMode.Projection);
 			GL.LoadIdentity();
-			GL.Ortho(0.0, level.Width, 0.0, level.Height, 0.0, 1.0);
+			var levelAspect = level.Width / (float)level.Height;
+			var windowAspect = windowWidth / (float)windowHeight;
+			var ratio = levelAspect / windowAspect;
+			if (levelAspect > windowAspect)
+			{
+				var delta = level.Height * 0.5f;
+				GL.Ortho(0.0, level.Width, 0, level.Height * ratio, 0.0, 1.0);
+			}
+			else
+			{
+				GL.Ortho(0.0, level.Width / ratio, 0.0, level.Height, 0.0, 1.0);
+			}
 			GL.MatrixMode(MatrixMode.Modelview);
 
 			GL.Clear(ClearBufferMask.ColorBufferBit);
 			GL.LoadIdentity();
 			GL.Enable(EnableCap.Blend);
 			GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
-			spriteSheet.Activate();
+			//spriteSheet.Activate();
 			GL.Color3(Color.White);
 			for (int x = 0; x < level.Width; ++x)
 			{
 				for (int y = 0; y < level.Height; ++y)
 				{
-					spriteSheet.Draw((uint)level.GetElement(x, y), new Box2D(x, y, 1.0f, 1.0f));
+					var rect = new Box2D(x, y, 1.0f, 1.0f);
+					//spriteSheet.Draw((uint)level.GetElement(x, y), rect);
+					var id = (int)level.GetElement(x, y);
+					var tex = tileSet.Textures[id];
+					tex.Activate();
+					rect.DrawTexturedRect(Box2D.BOX01);
+					tex.Deactivate();
 				}
 			}
 
-			spriteSheet.Deactivate();
+			//spriteSheet.Deactivate();
 			GL.Disable(EnableCap.Blend);
 		}
 
 		public void ResizeWindow(int width, int height)
 		{
 			GL.Viewport(0, 0, width, height);
+			windowWidth = width;
+			windowHeight = height;
 		}
 
 		private readonly SpriteSheet spriteSheet;
+		private readonly AnimationTextures tileSet = new AnimationTextures(5);
+		private int windowWidth;
+		private int windowHeight;
 	}
 }
