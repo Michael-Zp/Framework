4ead6963f2cc75dd6215f27f3aeb490e9cec764d
scherzer <scherzer@hs-weingarten.de>
2017-09-04 09:01:38 +0200

    quick-commit

---
+++ Tools/ProjToTemplate/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Build.Evaluation;
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ProjToTemplate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 2)
+			{
+				Console.WriteLine(nameof(ProjToTemplate) + " <sourceProjPath> <destTemplateZipPath>");
+				return;
+			}
+			var proj = new Project(args[0]);
+			//proj.Items
+			using (var zipFileStream = new FileStream(args[1], FileMode.Create))
+			{
+				var zip = new ZipArchive(zipFileStream, ZipArchiveMode.Create);
+				var entry = zip.CreateEntry("__templateIcon.ico");
+				//entry.Open().
+				//zip.CreateEntryFromFile();
+			}
+		}
+	}
+}
---
+++ Tools/ProjectResolveDMSDependencies/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Build.Evaluation;
+using System;
+
+namespace ProjectResolveDMSDependencies
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if(args.Length != 2)
+			{
+				Console.WriteLine(nameof(ProjectResolveDMSDependencies) + " <sourceProjPath> <destProjPath>");
+				return;
+			}
+			var proj = new Project(args[0]);
+			proj.RemoveProjRefs();
+			proj.RemovePackRef("OpenTK");
+			proj.RemovePackRef("NAudio");
+			proj.AddPackage("DMS.Common", "0.2.2");
+			proj.Save(args[1]);
+		}
+
+
+	}
+}
