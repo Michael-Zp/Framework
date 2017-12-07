89acb384076b74a7e6918229ef51dd0f54e334dc
scherzer <scherzer@hs-weingarten.de>
2017-12-07 18:56:00 +0100

    quick-commit

--- ACG/Examples/2D/DistanceEstimation.glsl
+++ ACG/Examples/2D/DistanceEstimation.glsl
@@ -43,26 +43,26 @@ void main()
 	//create uv to be in the range [0..1]²
 	vec2 uv = gl_FragCoord.xy / iResolution;
 	
-	float threshold = sin(iGlobalTime);
+	float threshold = abs(sin(iGlobalTime));
 
 	// range [-1..1]²
     uv = vec2(1) - 2 * uv;
 	//aspect correction
 	uv.x *= iResolution.x / iResolution.y;
-	uv *= 2;
+	uv *= 2; // range [-2..2]²
 	float pixelDelta = 4.0 / iResolution.y; //range / res
 	
 	float f = distField(uv);
-	float level = 0.0;
 	vec2 g = //uv.x < 0 ? gradGPU(uv, pixelDelta) : 
 	grad(uv, pixelDelta);
 	float de = abs(f) / length(g);
 
-	float subSet = f * 0.5;
+	float subSet = f;
 	// subSet = step(threshold, f);
+	// subSet = smoothstep(threshold + 0.0, threshold + 0.025, abs(f));
 	// subSet = de;
 	// subSet = clamp(de, 0.0, 0.025) * 20;
-	subSet = smoothstep(0.0, 0.025, de);
+	// subSet = smoothstep(0.0, 0.025, de);
 
 	float thickness = 0.05;
 	float d = distToInt(de * 5);
